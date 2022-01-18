7e70d08a7b3d999e7b32c2c68f0c58e49bea447c
zahaanshapoorjee <zahaan@shapoorjee.com>
2022-01-19 03:15:35 +0530

    what is the meaning of life?

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+console.log("RERESREJKASBF;AJF JAESFJLEF")
 ReactDOM.render(
   <React.StrictMode>
     <App />
