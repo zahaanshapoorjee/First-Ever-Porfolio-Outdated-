8e1e78891e00f73e536a1db1410bf90604bb0153
zahaanshapoorjee <zahaan@shapoorjee.com>
2022-02-03 15:52:32 +0530

    I am not happy

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -1,18 +1,18 @@
-import React, { useRef } from 'react';
+import React, { useRef,useState } from 'react';
 import { Link } from 'react-scroll/modules';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios'
 import emailjs from '@emailjs/browser';
 import{ init } from '@emailjs/browser';
 init("user_CvZDaEUr3zVvwuQ85PqYh");
 
 export default function ContactMe(){
   const form = useRef();
-  
-
+  const[info,setinfo]=useState({name:'',email:'',message:''})
   const sendEmail = (e) => {
     e.preventDefault();
-
+    console.log(info)
     emailjs.sendForm('service_traivfp', 'template_8wz0o0k', form.current, 'user_CvZDaEUr3zVvwuQ85PqYh')
       .then((result) => {
           console.log(result.text);
@@ -20,8 +20,29 @@ export default function ContactMe(){
           console.log(error.text);
       });
       alert('Message Sent Successfully!')
+      axios.post()
+      setinfo({name:'',email:'',message:''})
       form.current.reset()
   };
+  
+  const handleNameChange=(e)=>{
+    e.persist()
+    setinfo((info)=>({
+      ...info, name:e.target.value
+    }))
+  }
+  const handleEmailChange=(e)=>{
+    e.persist()
+    setinfo((info)=>({
+      ...info,email:e.target.value
+    }))
+  }
+  const handleMessageChange=(e)=>{
+    e.persist()
+    setinfo((info)=>({
+      ...info,message:e.target.value
+    }))
+  }
 
   return (
     <div className='daddy4'>
@@ -30,11 +51,11 @@ export default function ContactMe(){
     <div>
     <form ref={form} onSubmit={sendEmail} className='contactform'>
     <label>Name</label>
-     <input type="text" name="user_name" className='username'/><br></br>
+     <input type="text" name="user_name" className='username' value={info.name} onChange={handleNameChange} /><br></br>
      <label>Email</label>
-    <input type="email" name="user_email" className='emailid' /><br></br>
+    <input type="email" name="user_email" className='emailid' value={info.email} onChange={handleEmailChange} /><br></br>
     <label>Message</label>
-    <textarea name="message" className='messagetome' />
+    <textarea name="message" className='messagetome' value={info.message} onChange={handleMessageChange}/>
     <input type="submit" value="Send" className='contact-form-submit-button'/>
     </form>
     </div>
