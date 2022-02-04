b48a6e45f7539436817dcebf5d4111c9ead754ac
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2022-02-05 00:00:35 +0530

    I wonder what it is like to be happy...

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,7 +11,7 @@ export default function Projects()
         <h1 className='projectsText'>PROJECTS</h1>
         <div className='projects'>
            <div className='p1div' ><img  src={require('../Assets/project1.png')} alt='Failed to Load' className='project1'></img><h1 className='project1header'><u>Zahaan.com</u></h1>
-           <p id="project1info">Skills Used: HTML, CSS, JavaScript, React</p>
+           <p id="project1info">Skills Used: HTML, CSS, JavaScript, React, Express, Node.js, MongoDB</p>
            </div>
         </div>
         <div className='thirdscroll'><Link to='section4'smooth={true} ><button className='scrollButton2'><FontAwesomeIcon icon={faAngleDoubleDown} size="2x" className='scrollicon'></FontAwesomeIcon></button></Link></div>
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -5,17 +5,7 @@ import {faAngleDoubleUp,faAngleDoubleDown } from '@fortawesome/free-solid-svg-ic
 import SocialMedia from './SocialMedia';
 export default function SkillList()
 {
-    const c1style={
-    'background-color':'white',
-    height: '16.67%', 
-    width: '100%',
-    position: 'relative',
-    'border-radius': '25px',
-    'box-shadow': 'inset 0 -1px 1px rgba(255, 255, 255, 0.3)',
-    'margin-bottom': '10%',
-    top:'3%',
-       }
-    return (
+       return (
         <div className='daddy'>
          <div className='firstscrollup'><Link to='top'smooth={true} ><FontAwesomeIcon icon={faAngleDoubleUp} size="2x" className='firstscrollupicon'></FontAwesomeIcon></Link></div>   
         <h1 className='skillsText'>SKILLS</h1>
@@ -23,38 +13,21 @@ export default function SkillList()
         {/* flex direction row ^  */}
            <div className='skill'>
                {/* items flex direction column */}
-                     <div className="skillListItems"><i className="devicon-python-plain"></i>  Python</div>
-                     <div className="skillListItems"><i className="devicon-html5-plain"></i>  HTML</div>
-                     <div className="skillListItems"><i className="devicon-css3-plain"></i>  CSS</div>
-                     <div className="skillListItems"><i className="devicon-javascript-plain"></i>  JavaScript</div>
-                     <div className="skillListItems"><i className="devicon-react-plain"></i>  React</div>
-                     <div className="skillListItems"><i className="devicon-java-plain"></i>  Java</div>
-                     <div className="skillListItems"><i className="devicon-cplusplus-plain"></i>  C++</div>
+                     <div className="skillListItems"><i className="devicon-python-plain"></i><p className='mySkills'>Python</p></div>
+                     <div className="skillListItems"><i className="devicon-html5-plain"></i><p className='mySkills'>HTML</p></div>
+                     <div className="skillListItems"><i className="devicon-css3-plain"></i><p className='mySkills'>CSS</p></div>
+                     <div className="skillListItems"><i className="devicon-javascript-plain"></i><p className='mySkills'>JavaScript</p></div>
+                     <div className="skillListItems"><i className="devicon-react-plain"></i><p className='mySkills'>React.js</p></div>
+                     <div className="skillListItems"><i className="devicon-java-plain"></i><p className='mySkills'>Java</p></div>
+                     <div className="skillListItems"><i className="devicon-express-original"></i><p className='mySkills'>Express.js</p></div>
+                     <div className="skillListItems"><i className="devicon-nodejs-plain"></i><p className='mySkills'>Node.js</p></div>
+                     <div className="skillListItems"><i className="devicon-mongodb-plain"></i><p className='mySkills'>MongoDB</p></div>
+                     <div className="skillListItems"><i className="devicon-mysql-plain"></i><p className='mySkills'>MySQL</p></div>
+                     <div className="skillListItems"><i className="devicon-github-original-wordmark"></i><p className='mySkills'>Git</p></div>
+                     <div className="skillListItems"><i className="devicon-cplusplus-plain"></i><p className='mySkills'>C++</p></div>
+                     
            </div>
-           <div className='skill'>
-               {/* meters flex direction column */}
-               <div style={c1style}>
-	            <span className='meter1' id="firstmeter"></span>
-                </div>
-                <div style={c1style}>
-	            <span className='meter2' id="secondmeter"></span>
-                </div>
-                <div style={c1style}>
-                <span className='meter3' id="thirdmeter"></span>
-                </div>
-                <div style={c1style}>
-                <span className='meter4' id="fourthmeter"></span>
-                </div>
-                <div style={c1style}>
-                <span className='meter5' id="fifthmeter"></span>
-                </div>
-                <div style={c1style}>
-                <span className='meter6' id="sixthmeter"></span>
-                </div>
-                <div style={c1style}>
-                <span className='meter7' id="seventhmeter"></span>
-                </div>
-            </div>
+           
             
          </div>
          <div className='secondscroll'><Link to='section3'smooth={true} ><button className='scrollButton2'><FontAwesomeIcon icon={faAngleDoubleDown} size="2x" className='scrollicon'></FontAwesomeIcon></button></Link>
