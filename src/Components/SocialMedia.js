37c2d1154fc2c8655a392d7f52ff24f5f979762c
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2022-02-07 12:31:28 +0530

    I am in pain...

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -12,7 +12,7 @@ export default function SocialMedia()
             <div className="nav-bar"></div>
             <div id="socialIcons">
             <a href="https://www.youtube.com/channel/UCcpfcvB3qFEjwVaDA9fxb9A" target="_blank" rel="noreferrer" className='social-youtube'><FontAwesomeIcon icon={faYoutube} size="3x"></FontAwesomeIcon></a>
-            <a href="https://www.instagram.com/zahaanxshah" target="_blank" rel="noreferrer" className='social-instagram'><FontAwesomeIcon icon={faInstagram} size="3x"></FontAwesomeIcon></a>
+            <a href="https://www.instagram.com/zahaanshapoorjee/" target="_blank" rel="noreferrer" className='social-instagram'><FontAwesomeIcon icon={faInstagram} size="3x"></FontAwesomeIcon></a>
             <a href="https://www.linkedin.com/in/zahaan-shapoorjee-22a465193/" target="_blank" rel="noreferrer" className='social-linkedin'><FontAwesomeIcon icon={faLinkedin} size="3x"></FontAwesomeIcon></a>
             <a href="https://www.facebook.com/zahaan.shapoorjee/" target="_blank" rel="noreferrer" className='social-facebook'><FontAwesomeIcon icon={faFacebook} size="3x"></FontAwesomeIcon></a>
             </div>
