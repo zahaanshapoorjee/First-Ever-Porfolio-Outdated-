ad29491c2e07eef15964dae98e50670021df9655
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2022-02-08 09:08:03 +0530

    I wish I was happier

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,8 +10,13 @@ export default function Projects()
         <div className='firstscrollup'><Link to='section2'smooth={true} ><FontAwesomeIcon icon={faAngleDoubleUp} size="2x" className='firstscrollupicon'></FontAwesomeIcon></Link></div>
         <h1 className='projectsText'>PROJECTS</h1>
         <div className='projects'>
-           <div className='p1div' ><img  src={require('../Assets/project1.png')} alt='Failed to Load' className='project1'></img><h1 className='project1header'><u>Zahaan.com</u></h1>
-           <p id="project1info">Skills Used: React.js, Express.js, Node.js, MongoDB</p>
+           <div className='pdiv' ><img  src={require('../Assets/project1.png')} alt='Failed to Load' className='project'></img><h1 className='projectheader'><u>Portfolio Website</u></h1>
+           <p id="projectinfo">Skills Used: React.js, Express.js, Node.js, MongoDB</p>
+           <p id="projectinfo">Hosted on: <a className='link' href='https://zahaan.netlify.app/'>https://zahaan.netlify.app/</a></p>
+           </div>
+           <div className='pdiv' ><img  src={require('../Assets/project2.png')} alt='Failed to Load' className='project'></img><h1 className='projectheader'><u>Online Chatroom App</u></h1>
+           <p id="projectinfo">Skills Used: React.js, Express.js, Node.js, Socket.IO</p>
+           <p id="projectinfo">Hosted on: <a className='link' href='https://zahaanshapoorjee-chat-app.netlify.app/'>https://zahaanshapoorjee-chat-app.netlify.app/</a></p>
            </div>
         </div>
         <div className='thirdscroll'><Link to='section4'smooth={true} ><button className='scrollButton2'><FontAwesomeIcon icon={faAngleDoubleDown} size="2x" className='scrollicon'></FontAwesomeIcon></button></Link></div>
